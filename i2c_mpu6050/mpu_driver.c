1aca9bff924cf3f42168b46bfa878a1a5effa269
Moafk Aljabi <mwafak_sh@hotmail.com>
2025-09-14 22:59:31 +0100

    mpu6050 README.md

---
+++ i2c_mpu6050/mpu_driver.c
@@ -0,0 +1,42 @@
+/*
+ *
+ * 2025 
+ * MA 
+ * License 
+ *
+ * Description: I2C Linux driver for the MPU6050 
+ * Tested with the Raspberry Pi 4b. Linux 6.12
+ *
+ * For now the module uses ... 
+ * 
+ *
+ * */
+
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/sysfs.h>
+#include <linux/i2c.h>
+#include <linux/slab.h>
+#include <linux/fs.h>
+#include <linux/err.h>
+
+
+
+/*
+ *
+ *  struct to 
+ *
+ *
+ * */
+
+
+
+
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("I2C driver for the MPU6050");
+MODULE_AUTHOR("MA");
+MODULE_VERSION("0.1");
+
+
